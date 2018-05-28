b9a4b9f07f79a709f88ba4d856b1467cc579a0a8
SAPIENT\agu223 <agupta249@sapient.com>
2018-05-28 16:35:33 +0530

    Added production route

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 const authRoutes = require("./routes/sampleRoutes");
 authRoutes(app);
 
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const port = process.env.PORT || 9999;
 app.listen(port);
